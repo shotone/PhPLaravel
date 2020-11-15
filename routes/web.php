e0d88128b8514321d6b252b8f708ff3769f93aa7
shotone <shota.firanashvili.1@btu.edu.ge>
2020-11-15 17:45:27 +0400

    Added Authorization & Register functions

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,29 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home')->middleware('auth');
 
 
-Route::get('/posts', [\App\Http\Controllers\PostController::class,'index']);
-Route::get('/posts/create', [\App\Http\Controllers\PostController::class,'create'])->name('posts.create');
-Route::get('/posts/{post}', [\App\Http\Controllers\PostController::class,'show'])->name('posts.show');
-Route::post('/posts/save_post', [\App\Http\Controllers\PostController::class,'save'])->name('posts.save');
-Route::get('/posts/{post}/edit', [\App\Http\Controllers\PostController::class,'edit'])->name('posts.edit');
-Route::put('/posts/{post}/update', [\App\Http\Controllers\PostController::class,'update'])->name('posts.update');
-Route::delete('/posts/{post}/delete', [\App\Http\Controllers\PostController::class,'delete'])->name('posts.delete');
+Route::get('/posts', [\App\Http\Controllers\PostController::class,'index'])->middleware('auth');;
+Route::get('/posts/create', [\App\Http\Controllers\PostController::class,'create'])->name('posts.create')->middleware('auth');;
+Route::get('/posts/{post}', [\App\Http\Controllers\PostController::class,'show'])->name('posts.show')->middleware('auth');;
+Route::post('/posts/save_post', [\App\Http\Controllers\PostController::class,'save'])->name('posts.save')->middleware('auth');;
+Route::get('/posts/{post}/edit', [\App\Http\Controllers\PostController::class,'edit'])->name('posts.edit')->middleware('auth');;
+Route::put('/posts/{post}/update', [\App\Http\Controllers\PostController::class,'update'])->name('posts.update')->middleware('auth');;
+Route::delete('/posts/{post}/delete', [\App\Http\Controllers\PostController::class,'delete'])->name('posts.delete')->middleware('auth');;
+
+
+Route::post('/register', [\App\Http\Controllers\LoginController::class,'register'])->name('register');
+Route::get('/register_view', [\App\Http\Controllers\LoginController::class,'register_view'])->name('register_view');
+Route::get('/register/login', [\App\Http\Controllers\LoginController::class,'login'])->name('login');
+Route::post('/register/post-login', [\App\Http\Controllers\LoginController::class,'postlogin'])->name('post_login');
+Route::get('/register/logout', [\App\Http\Controllers\LoginController::class,'logout'])->name('logout');
+
+
 //    $posts = DB::table('posts')->get();
 //    return $posts;
 //    return view('posts')->with('posts', $posts);
+
+//Route::middleware('auth')->group(function (){
+//
+//});
